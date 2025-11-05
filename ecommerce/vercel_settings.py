b9eb665435537248255a9e7c2963a70f3d1c6d5f
Jan Christopher Francisco <jcfrancisco@student.apc.edu.ph>
2025-11-05 17:21:21 +0800

    enable errors

--- ecommerce/vercel_settings.py
+++ ecommerce/vercel_settings.py
@@ -48,3 +48,38 @@ WHITENOISE_AUTOREFRESH = True
 
 # Use cookie-based sessions to avoid DB writes during cold starts
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
+# Log server errors to console so Vercel shows tracebacks
+DEBUG_PROPAGATE_EXCEPTIONS = True
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'simple': {
+            'format': '[%(levelname)s] %(name)s: %(message)s'
+        }
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+        'django.server': {
+            'handlers': ['console'],
+            'level': 'ERROR',
+            'propagate': False,
+        },
+    },
+}
